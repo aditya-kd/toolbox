d6d4bb976a08ed0a77c06c2d20d0ab9d3f9e2a28
Aditya Kumar Sharma <74445022+aditya-kd@users.noreply.github.com>
2022-10-28 14:09:02 +0530

    Update moveMouse.js

--- moveMouse.js
+++ moveMouse.js
@@ -1,38 +1,41 @@
 var robot = require('robotjs')
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+
 
 function move(){
-    
-robot.setMouseDelay(2);
 
 var twoPI = Math.PI*2.0;
 var screenSize = robot.getScreenSize();
 var height = (screenSize.height/2)-10;
 var width = screenSize.width;
 
-for(var x=width/4; x<width/2; x*=2){
-
-    // Sine Wave
-    // y = (height/4) * Math.sin((twoPI*x)/(width/4))+(height/4);
-    // robot.moveMouse(x,y)
-    // robot.mouseClick("right");
-    // robot.setMouseDelay(2000);
-
     //Straight line
+    
     x = width/4;
     y = height/4;
-    console.log("Mouse Position: ", x, y);
-    robot.setMouseDelay(2000);
     robot.moveMouse(x,y);
+    console.log("Mouse Position: ", x, y);
     robot.mouseClick("right")
+    sleep(1000)
     robot.mouseClick("left")
+    
     x = width-width/4;
     y = height-height/4
     console.log("Mouse Position: ", x, y);
-    robot.setMouseDelay(2000);
-    robot.moveMouse(x,y);
+    robot.moveMouseSmooth(x,y);
     robot.mouseClick("right")
+    robot.moveMouseSmooth(x,y+80)
+    robot.moveMouseSmooth(x+20, y+80)
     robot.mouseClick("left")
+    console.log("Sleeping, You have mouse control now")
+    sleep(5000)
     
 }
-}
-setInterval(move, 0);
+setInterval(move, 1000);
